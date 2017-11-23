af23e58e3c29f2bc33d68e8ad82853533095f697
Ethan Fung <EthanEFung@gmail.com>
2017-11-22 16:08:26 -0800

    deprecate:

--- client/js/models.js
+++ client/js/models.js
@@ -1,17 +1,14 @@
 // TODO: BUILD MODELS
 class Game {
-  constructor() {
-    this.board = this.renderBoard(5);
+  constructor(n) {
+    this.board = this.renderBoard(n);
     this.turn = "white";
     this.winner = null;
-
-    this.start(this.board);
   }
   renderBoard(n) {
     if (n > 8) throw new Error("cannot render a board bigger than 8 spaces");
     const board = [];
     const colNotation = "abcdefgh";
-
     for (let i = 0; i < n; i++) {
       const row = [];
       for (let j = 0; j < n; j++) {
@@ -21,11 +18,27 @@ class Game {
     }
     return board;
   }
-  start() {}
+  start() {
+    const firstRow = this.board[0];
+    const lastRow = this.board[this.board.length - 1];
+
+    firstRow.forEach(sq => (sq.piece = new Pawn("black", sq.coord)));
+    lastRow.forEach(sq => (sq.piece = new Pawn("white", sq.coord)));
+    // window.alert("game has begun!");
+  }
 }
 
-class Pawn {}
+class Pawn {
+  constructor(side, coord) {
+    this.side = side;
+    this.coord = coord;
+  }
+  advance(currentCoord, desiredCoord, board) {
+    const colNotation = "abcdefgh";
+  }
+  capture() {}
+}
 
 class Player {}
 
-module.exports = { Game };
+module.exports = { Game, Pawn, Player };
--- test/models.test.js
+++ test/models.test.js
@@ -1,9 +1,9 @@
-const { Game } = require("../client/js/models");
+const { Game, Pawn, Player } = require("../client/js/models");
 
 /**
  * const actual;
  * const expected;
- * expect(actual).toEqual
+ * expect(actual).toEqual(expected)
  *
  * const actual;
  * expect(actual).toBeNull
@@ -31,5 +31,71 @@ describe("Game", function() {
       ]
     ];
     expect(actual).toEqual(expected);
+  });
+
+  it("should set up pieces to start", function() {
+    const test = new Game(3);
+    test.start();
+    const actual = test.board;
+
+    const expected = [
+      [
+        { piece: new Pawn("black", "a3"), coord: "a3" },
+        { piece: new Pawn("black", "b3"), coord: "b3" },
+        { piece: new Pawn("black", "c3"), coord: "c3" }
+      ],
+      [
+        { piece: null, coord: "a2" },
+        { piece: null, coord: "b2" },
+        { piece: null, coord: "c2" }
+      ],
+      [
+        { piece: new Pawn("white", "a1"), coord: "a1" },
+        { piece: new Pawn("white", "b1"), coord: "b1" },
+        { piece: new Pawn("white", "c1"), coord: "c1" }
+      ]
+    ];
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("Pawns", function() {
+  it("should have a designated side", function() {
+    const actual = new Pawn("black").side;
+    const expected = "black";
+    expect(actual).toEqual(expected);
+  });
+
+  it("should have a designated coord", function() {
+    const actual = new Pawn("white", "a1").coord;
+    const expected = "a1";
+    expect(actual).toEqual(expected);
+  });
+
+  it("should be able to advance to an empty space", function() {
+    const game = new Game(3);
+    game.start();
+    const pawn = game.board[0][0].piece;
+    pawn.advance(pawn.coord, "a2", game.board);
+
+    const expected = [
+      [
+        { piece: null, coord: "a3" },
+        { piece: new Pawn("black", "b3"), coord: "b3" },
+        { piece: new Pawn("black", "c3"), coord: "c3" }
+      ],
+      [
+        { piece: new Pawn("black", "a2"), coord: "a2" },
+        { piece: null, coord: "b2" },
+        { piece: null, coord: "c2" }
+      ],
+      [
+        { piece: new Pawn("white", "a1"), coord: "a1" },
+        { piece: new Pawn("white", "b1"), coord: "b1" },
+        { piece: new Pawn("white", "c1"), coord: "c1" }
+      ]
+    ];
+
+    expect(game.board).toEqual(expected);
   });
 });
